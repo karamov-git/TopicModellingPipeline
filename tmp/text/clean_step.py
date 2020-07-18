0a2dd36deafa555ca9db5167d773131ce1f44a6a
karamov-git <ybiz177@gmail.com>
2020-07-18 14:30:07 +0500

    add faster mode for RuLemmatizationCleanStep

--- tmp/text/clean_step.py
+++ tmp/text/clean_step.py
@@ -106,18 +106,42 @@ class ApplyFunctionForDocumentCleanStep(BaseCleanStep):
 
 class RuLemmatizationCleanStep(BaseCleanStep):
 
-    def __init__(self):
+    def __init__(self, faster=False):
         super().__init__()
         self.analyzer = Mystem()
-        self.count = 0
+        self.faster = faster
 
     def __call__(self, *args, **kwargs):
         documents_collection = super().__call__(*args)
+        if self.faster:
+            return self.__lemmatize_faster(documents_collection)
         return documents_collection.apply(self.__lemmatize)
+
+    def __lemmatize_faster(self, documents: pd.Series):
+
+        def lemmatize_merged_documents_by_br_and_split_after(texts, analyzer):
+            lol = lambda lst, sz: [lst[i:i + sz] for i in range(0, len(lst), sz)]
+            txtpart = lol(texts, 1000)
+            res = []
+            for txtp in txtpart:
+                alltexts = ' '.join([txt + ' br ' for txt in txtp])
+
+                words = analyzer.lemmatize(alltexts)
+
+                doc = []
+                for txt in words:
+                    if txt != '\n' and txt.strip() != '':
+                        if txt == 'br':
+                            res.append(doc)
+                            doc = []
+                        else:
+                            doc.append(txt)
+            return res
+
+        cleaned_data = lemmatize_merged_documents_by_br_and_split_after(documents.to_list(), self.analyzer)
+        return pd.Series(cleaned_data, index=documents.index)
 
     def __lemmatize(self, document):
         lemmatization_document = [word for word in self.analyzer.lemmatize(document) if
                                   word not in ['', '\n', '\t', None]]
-        self.count = self.count + 1
-        print(self.count)
         return ''.join(lemmatization_document)
